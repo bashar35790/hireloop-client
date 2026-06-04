f1bdf941d9c0ec94662b823d8c315ff72acde97f
bashar <bashar35790@gmail.com>
2026-06-04 16:13:54 +0600

    added hero banner section

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -22,7 +22,7 @@ interface NavbarProps {
 const maxWidthClasses = {
   sm: "max-w-[640px]",
   md: "max-w-[768px]",
-  lg: "max-w-[1024px]",
+  lg: "max-w-[1280px]",
   xl: "max-w-[1280px]",
   "2xl": "max-w-[1536px]",
   full: "max-w-full",
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,10 @@
 
+import HeroBanner from "./components/HeroBanner";
+
 export default function Home() {
   return (
-    <div className="flex flex-col flex-1 items-center justify-center w-full bg-gray-100">
-      <main className="flex flex-1 w-full max-w-7xl flex-col items-center justify-between py-32 px-16 d sm:items-start">
-          <h1  className="text-5xl text-orange-700">Hello</h1>
-      </main>
-    </div>
+    <main className="flex flex-col flex-1 w-full">
+      <HeroBanner />
+    </main>
   );
 }
